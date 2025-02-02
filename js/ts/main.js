881d360592e87a24030ff7cb26856c90267041b3
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2025-02-02 07:25:54 +0000

    Completed footer section. #1

--- js/ts/main.js
+++ js/ts/main.js
@@ -3,14 +3,13 @@
 const $navLinks = document.querySelector('.nav-links');
 const $navElement = document.querySelectorAll('.nav-element');
 const $heartIcon = document.querySelector('.heart-icon');
-const $userIcon = document.querySelector('.user-icon');
 if (!$navLinks) throw new Error('$navLinks query failed.');
 if (!$navElement) throw new Error('$navElement query failed.');
 if (!$heartIcon) throw new Error('$heartIcon query failed.');
-if (!$userIcon) throw new Error('$userIcon query failed.');
 // added an eventListener to the nav-bar to update color of nav-elements
 $navLinks.addEventListener('click', (event) => {
   const $eventTarget = event.target;
+  console.log($eventTarget);
   if ($eventTarget.matches('.nav-element.cursor')) {
     for (let i = 0; i < $navElement.length; i++) {
       if ($eventTarget === $navElement[i]) {
@@ -22,11 +21,6 @@ $navLinks.addEventListener('click', (event) => {
     }
   } else if ($eventTarget === $heartIcon) {
     $heartIcon.classList.add('active-icon');
-    for (let i = 0; i < $navElement.length; i++) {
-      $navElement[i].classList.remove('active-nav-element');
-    }
-  } else if ($eventTarget === $userIcon) {
-    $heartIcon.classList.remove('active-icon');
     for (let i = 0; i < $navElement.length; i++) {
       $navElement[i].classList.remove('active-nav-element');
     }
--- ts/main.ts
+++ ts/main.ts
@@ -2,15 +2,16 @@
 const $navLinks = document.querySelector('.nav-links');
 const $navElement = document.querySelectorAll('.nav-element');
 const $heartIcon = document.querySelector('.heart-icon');
+const $userIcon = document.querySelector('.user-icon');
 
 if (!$navLinks) throw new Error('$navLinks query failed.');
 if (!$navElement) throw new Error('$navElement query failed.');
 if (!$heartIcon) throw new Error('$heartIcon query failed.');
+if (!$userIcon) throw new Error('$userIcon query failed.');
 
 // added an eventListener to the nav-bar to update color of nav-elements
 $navLinks.addEventListener('click', (event) => {
   const $eventTarget = event.target as HTMLAnchorElement;
-  console.log($eventTarget);
   if ($eventTarget.matches('.nav-element.cursor')) {
     for (let i = 0; i < $navElement.length; i++) {
       if ($eventTarget === $navElement[i]) {
@@ -22,6 +23,11 @@ $navLinks.addEventListener('click', (event) => {
     }
   } else if ($eventTarget === $heartIcon) {
     $heartIcon.classList.add('active-icon');
+    for (let i = 0; i < $navElement.length; i++) {
+      $navElement[i].classList.remove('active-nav-element');
+    }
+  } else if ($eventTarget === $userIcon) {
+    $heartIcon.classList.remove('active-icon');
     for (let i = 0; i < $navElement.length; i++) {
       $navElement[i].classList.remove('active-nav-element');
     }
